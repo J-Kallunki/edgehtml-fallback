3169d872a55330ad859d7032a624d8f70c8519c8
Joonas Kallunki <jkallunki@gmail.com>
2019-10-24 08:04:52 +0300

    Create removeStyleTags function

--- index.js
+++ index.js
@@ -31,6 +31,41 @@ function ifDevelopEnvironment() {
   return false;
 }
 
+function removeTags(tags) {
+  if (
+    HTMLCollection.prototype.isPrototypeOf(tags) ||
+    NodeList.prototype.isPrototypeOf(tags) ||
+    ifNodeList(tags)
+  ) {
+    var devEnv = ifDevelopEnvironment();
+    for (var i = 0; i < tags.length; i++) {
+      var el = tags[i];
+      if (devEnv && console !== undefined) {
+        console.log("Removing element:", el);
+      }
+      el.parentNode.removeChild(el);
+    }
+    return true;
+  }
+  return false;
+}
+
+function removeStyleTags(element) {
+  if (element === false || element === undefined || element === null) {
+    return false;
+  }
+  var tags = element.getElementsByTagName('style');
+  if (tags !== undefined && tags.length > 0) {
+    removeTags(tags);
+  } else {
+    var devEnv = ifDevelopEnvironment();
+    if (devEnv && console !== undefined) {
+      console.log("No style-tags found.");
+    }
+  }
+  return true;
+}
+
 function getElement(el) {
   if (typeof window === 'undefined') {
     return false;
